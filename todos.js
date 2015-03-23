8b512bc701021a9bd6e53f65d61e6a1de5b0d89c
AJ <aj.nice@daumkakao.com>
2015-03-23 10:46:50 +0900

    함수화

--- todos.js
+++ todos.js
@@ -3,7 +3,11 @@
 //개발자도구에서 메세지확인
 console.log('todos.js');
 
+function getDom(id){
+  return document.getElementById(id);
+}
+
 // id가 todoString 인 element 가져오기
-var todoStringField = document.getElementById('todoString');
+var todoStringField = getDom('todoString');
 
 console.log( todoStringField);
