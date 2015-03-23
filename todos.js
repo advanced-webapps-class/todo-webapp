591656662c897ca0a59a15ad677a21ab6490902e
AJ <aj.nice@daumkakao.com>
2015-03-23 11:12:23 +0900

    event 핸들러 콜백함수 처리

--- todos.js
+++ todos.js
@@ -3,36 +3,40 @@
 //개발자도구에서 메세지확인
 console.log('todos.js');
 
-function getDom(id){
+function getDom(id) {
   return document.getElementById(id);
 }
 
 // id가 todoString 인 element 가져오기
 var todoStringField = getDom('todoString');
 
-console.log( todoStringField);
+console.log(todoStringField);
 
-todoStringField.addEventListener('keyup', function (event) {
+
+todoStringField.addEventListener('keyup', addTodo);
+
+
+function addTodo(event) {
 
   console.log('todoStringField keyup!');
 
-  //엔터키를 입력하면
-  if(event.keyCode === 13){
+  //엔터키가 아니면 함수 중지
+  if (event.keyCode !== 13) {
+    event.stopPropagation(); //이벤트 전파중지
+    return;
+  }
 
-    var newTodo = todoStringField.value;
-    todoStringField.value = "";
+  var newTodo = todoStringField.value;
+  todoStringField.value = "";
 
-    console.log('새로운 todo:', newTodo);
+  console.log('새로운 todo:', newTodo);
 
-    var listDom = getDom('todoList');
+  var listDom = getDom('todoList');
 
-    listDom.innerHTML += ' \
+  listDom.innerHTML += ' \
         <li>\
             <button class="delete">×</button>\
             <input type="checkbox" class="toggle-checked">\
-            <span class="text"> '+ newTodo +' </span>\
+            <span class="text"> ' + newTodo + ' </span>\
         </li>';
-  }
-
-
-});
+}
