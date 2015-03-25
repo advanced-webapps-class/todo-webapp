63f28015557a8d2b03bc8ba8e0151e3127bf8728
AJ <aj.nice@daumkakao.com>
2015-03-25 20:30:00 +0900

    삭제버튼 이벤트 주기

--- todos.js
+++ todos.js
@@ -15,5 +15,8 @@ console.log(todoStringField);
 
 todoStringField.addEventListener('keyup', addTodo);
 
+//삭제버튼 이벤트 잡기 위해 상위에서 이벤트 listen 하기
+listDom.addEventListener('click', checkDelete);
+
 //처음 로딩시에 기존에 저장된 데이터 가져와서 보여주기
 listDom.innerHTML = loadData();
--- util.js
+++ util.js
@@ -8,7 +8,7 @@ function addTodo(event) {
   console.log('todoStringField keyup!');
 
   //엔터키가 아니면 함수 중지
-  if (event.keyCode !== 13) {
+  if (event.keyCode !== 13 || todoStringField.value === "") {
     event.stopPropagation(); //이벤트 전파중지
     return;
   }
@@ -20,14 +20,31 @@ function addTodo(event) {
 
   listDom.innerHTML += tmpl(todoTemplateHtml, {todo: newTodo});
 
-  saveData(listDom.innerHTML);
+  saveData();
+}
+function checkDelete(event) {
+
+  //if(!confirm('정말 삭제하나용? ')){
+  //  return;
+  //}
+
+
+  if(event.target.className !== 'delete') {
+    return;
+  }
+  var deleteBtn = event.target;
+
+  //버튼의 상위엘리먼트 <li> 제거
+  deleteBtn.parentElement.remove();
+
+  saveData();
 }
 
 function loadData() {
   console.log('loadData()');
   return localStorage.getItem('todoHtml');
 }
-function saveData(html) {
+function saveData() {
   console.log('saveData()');
-  localStorage.setItem('todoHtml', html);
+  localStorage.setItem('todoHtml', listDom.innerHTML);
 }
